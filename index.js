dee001c1726eb07245f9e03765ca89eeed50b110
K Sadov <ksadov@gmail.com>
2020-07-01 20:44:46 -0500

    fix crashing with a try/catch loop, lol

--- index.js
+++ index.js
@@ -10,15 +10,29 @@ const runWasm = async () => {
 
     var ctx = canvas.getContext('2d');
     ctx.imageSmoothingEnabled = false;
-    var pixelsPtr = rustWasm.render_stranger();
-    var pixelArray = new Uint8ClampedArray(rustWasm.memory.buffer, pixelsPtr, 4 * width * height);
+    var unrendered = true;
+    var pixelsPtr;
+    /* I'm just not going to bother figuring out why the rendering crashes 2%
+       of the time */
+    while (unrendered) {
+	try {
+	    pixelsPtr = rustWasm.render_stranger();
+	    unrendered = false;
+	}
+	catch (err) {}
+    }
+    
+    var pixelArray = new Uint8ClampedArray(rustWasm.memory.buffer,
+					   pixelsPtr,
+					   4 * width * height);
 
     ctx.putImageData(new ImageData(pixelArray, width, height), 0, 0);
 
     gen.addEventListener('click', function() {
 	console.log("x");
 	    pixelsPtr = rustWasm.render_stranger();
-	pixelArray = new Uint8ClampedArray(rustWasm.memory.buffer, pixelsPtr, 4 * width * height);
+	pixelArray = new Uint8ClampedArray(rustWasm.memory.buffer,
+					   pixelsPtr, 4 * width * height);
 	ctx.putImageData(new ImageData(pixelArray, width, height), 0, 0);
 	}, false);
 }    
